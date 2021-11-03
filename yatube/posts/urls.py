df6dc521ed9e04d1532defcec2281a70e1e3a176
Серегей Токарев <sat0304@yandex.ru>
2021-11-03 23:36:07 +0400

    Urls.py modified

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -22,5 +22,5 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('groups/<slug:slug>/', views.group_posts, name='group_posts')
+    path('group/<slug:slug>/', views.group_posts, name='group_posts')
 ]
