f80eda3fa1f21be3f2915eda0db0279165628717
Серегей Токарев <sat0304@yandex.ru>
2021-11-04 22:40:12 +0400

    Добавлены L Tolstoy

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -7,5 +7,10 @@ def index(request):
     template = 'posts/index.html'
     return render(request, template)
 
+def index(request):
+    """Страница Л.Н. Толстого."""
+    template = 'posts/group_list.html'
+    return render(request, template)
+
 def group_posts(request, slug):
-    return HttpResponse(f'Здесь решается судьба мира {slug}')
+    return HttpResponse('Здесь решается судьба мира', slug)
