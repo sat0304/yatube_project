52fc2376d809bd56b87ac768f7d4f745185887d5
Серегей Токарев <sat0304@yandex.ru>
2021-11-07 00:11:13 +0400

    Добавлены fav.ico

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -22,5 +22,6 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('group/<slug:slug>/', views.group_posts, name='group_posts')
+    path('group/<slug:slug>/', views.group_posts, name='group_posts'),
+    path('img/fav/fav.ico/', views.icons, name='icons')
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth import logout as core_logout
 from django.shortcuts import render
 from typing import Dict
 
 context = {
            'title_index':'Это главная страница проекта Yatube',
-           'title_groups':'Здесь будет информация о группах проекта Yatube'
+           'title_groups':'Здесь будет информация о группах проекта Yatube',
+           'title_icon':'Здесь находяится коллекция иконок проекта Yatube'
           }
 
 
@@ -15,4 +17,9 @@ def index(request):
 def group_posts(request):
     """Cтраница  публикаций."""
     template = 'posts/group_list.html'
+    return render(request, template, context)
+
+def icons(request):
+    """Коллекция иконок."""
+    template = 'img/fav/fav.ico'
     return render(request, template, context)
