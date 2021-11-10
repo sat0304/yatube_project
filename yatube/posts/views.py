ddba9e8dcc9cc760e0228004c4992e494c72c82c
sat0304 <73336464+sat0304@users.noreply.github.com>
2021-11-10 22:52:04 +0400

    Update views.py

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,20 +1,33 @@
-from django.contrib.auth import logout as core_logout
-from django.shortcuts import render
-from typing import Dict
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
-context = {
-           'title_index':'Это главная страница проекта Yatube',
-           'title_groups':'Здесь будет информация о группах проекта Yatube',
-           'title_icon':'Здесь находяится коллекция иконок проекта Yatube'
-          }
-
-
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
 def index(request):
-    """Главная страница."""
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
     template = 'posts/index.html'
-    return render(request, template, context)
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, template, context) 
 
-def group_posts(request):
+
+def group_posts(request, slug):
     """Cтраница  публикаций."""
     template = 'posts/group_list.html'
-    return render(request, template, context)
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context) 
